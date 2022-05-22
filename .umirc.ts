2c3fdcc8167def4f269d102551d264d819aff5fe
huabuyu1993 <105578007+huabuyu1993@users.noreply.github.com>
2022-05-22 20:18:54 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs',
   mode: 'site',
-  base: '/docs/',
-  publicPath: '/docs/',
+  base: '/site/',
+  publicPath: '/site/',
   // publicPath: process.env.NODE_ENV === 'production' ? '/foo/' : '/',
   // more config: https://d.umijs.org/config
 });
